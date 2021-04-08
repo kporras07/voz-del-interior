4d2a4f7b1f7152a788ca9990ca96d8901048a50c
Kevin Porras <kporras07@gmail.com>
2021-04-07 21:26:25 -0600

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: '1rem',
             }}
           >
-            Latest Stories
+            Últimas entradas
           </h1>
         </div>
         <section className="section">
